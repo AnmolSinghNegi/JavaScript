7df3090ae0e2dbdc9c6bdb928293b0a16c36f755
AnmolSinghNegi <99644009+AnmolSinghNegi@users.noreply.github.com>
2024-02-06 14:23:11 +0000

    variables

---
+++ 01-Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId="06";
+let accountEmail="anmolnegi242@gmail.com";
+var accountPassword="12345";
+accountCity="Dehradun"//Since js is well-formed language it can handle these but not recommended;
+//accountId=2;//Cannot change constants;
+accountEmail="anmolsinghnegi06@gmail.com";
+accountPassword="233444";
+accountCity="Bengaluru";
+let accountState;//By default undefined;
+console.log(accountId);//print normally
+console.table([accountEmail,accountPassword,accountCity,]);
+/* Prefer not to use var because of issue in block scope and funtional scope(Hosting)
+*/
