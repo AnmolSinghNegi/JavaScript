f873bd34f8b8ec795143e98302378cbf0d1dc02d
AnmolSinghNegi <99644009+AnmolSinghNegi@users.noreply.github.com>
2024-02-06 16:28:18 +0000

    datatype

---
+++ 01-Basics/datatype& ECMA standards.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+//alert("Hello") won't work since I am using nodejs
+let name="Anmol";//string
+let age=18;//number
+let isLogedIn=false;//boolean
+/*
+Some the datatype
+  number
+  bigint
+  strings
+  boolean
+  null-> standalone value;
+  undefined->value not assigned
+  symbol->unique
+  object
+*/
+console.log(typeof "Anmol")//string
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
