34cdabd258dc62e31fec6c51bda5d83a9019e8c6
AnmolSinghNegi <99644009+AnmolSinghNegi@users.noreply.github.com>
2024-02-08 02:06:39 +0000

    while loop

---
+++ 05_iterations/02-whileLoop.js
@@ -0,0 +1,20 @@
+//while
+let index=0
+while(index<=10){
+    console.log(`Value of index is ${index}`);
+    index+=2;
+}
+//array with while loop
+let myArray=["Geats","Zi-O","Saber"];
+let arr =0;
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+//do-while loop
+let score=11;
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score<=10)
